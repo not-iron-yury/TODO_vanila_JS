c98b2b70446e0a88216247de22379aceb2734ee3
Yury Notiron <not.iron.yuriy@inbox.ru>
2024-11-23 19:19:26 +0300

    Тодошка на ванильном JS. Небольшая практика с LocalStorage, и событиями чуть сложнее, чем один клик.
    До этого реализовывал подобную, с немного меньшим
    функционалом на реакте. В процессе сравнивал разницу.
    
    Changes to be committed:
    	new file:   checkmark.svg
    	new file:   index.html
    	new file:   index.js
    	new file:   styles.css

---
+++ index.js
@@ -0,0 +1,202 @@
+const todoFormElem = document.querySelector('.form');
+const todoInputElem = document.querySelector('.formInput');
+const todoListElem = document.querySelector('.list');
+const donedTodosElem = document.querySelector('.donedTodos');
+
+// ------------------- LocalStorage ------------------- //
+const setItemToLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+const removeItemFromLocalStorage = key => {
+  localStorage.removeItem(key);
+};
+const getItemFromLocalStorage = key => {
+  return JSON.parse(localStorage.getItem(key));
+};
+// ------------------- /LocalStorage ------------------- //
+
+const todos = {
+  _count: getItemFromLocalStorage('TODO_counter') || 0,
+  _items: getItemFromLocalStorage('TODO_list') || [],
+
+  get count() {
+    return this._count;
+  },
+  set count(newValue) {
+    this._count = newValue;
+    todosCountDonedOutput();
+    setItemToLocalStorage('TODO_counter', this._count);
+  },
+
+  get items() {
+    return this._items;
+  },
+  set items(newItems) {
+    this._items = newItems;
+    setItemToLocalStorage('TODO_list', this._items);
+    renderTodoList(todos.items);
+  },
+};
+
+// todos.items (вывод на экран)
+function renderTodoList(todos) {
+  todoListElem.innerHTML = null; // обнуляем список, что бы пункты не повторялись
+
+  todos.forEach(elem => {
+    todoListElem.append(createTodoElem(elem.key, elem.value, elem.doned));
+  });
+}
+
+// todo.count (вывод на экран)
+const todosCountDonedOutput = () => {
+  donedTodosElem.textContent = todos.count;
+};
+
+// инициализация списка todos после загрузки DOM
+document.addEventListener('DOMContentLoaded', () => {
+  todosCountDonedOutput();
+  renderTodoList(todos.items);
+});
+
+// create data-key
+const createDataKey = () => {
+  return Date.now();
+};
+
+// createTodoElem
+function createTodoElem(key, value, doned = false) {
+  const todoItem = document.createElement('li');
+  todoItem.setAttribute('class', 'item');
+  todoItem.setAttribute('data-key', key);
+  doned && todoItem.setAttribute('data-doned', '');
+
+  const todoItemText = document.createElement('p');
+  todoItemText.addEventListener('click', doneTodo);
+  todoItemText.setAttribute('class', doned ? 'todotext completed' : 'todotext');
+  todoItemText.textContent = value;
+
+  const todoButtons = document.createElement('div');
+  todoButtons.setAttribute('class', 'todoButtons');
+
+  const todoRemoveBtn = document.createElement('button');
+  todoRemoveBtn.addEventListener('click', removeTodo);
+  todoRemoveBtn.setAttribute('class', 'btn_remove');
+  todoRemoveBtn.innerHTML = '&times';
+
+  const todoEditBtn = document.createElement('button');
+  todoEditBtn.addEventListener('click', editTodo);
+  todoEditBtn.setAttribute('class', 'btn_edit');
+  todoEditBtn.innerHTML = '&#10000';
+
+  todoButtons.append(todoRemoveBtn);
+  todoButtons.append(todoEditBtn);
+
+  todoItem.append(todoItemText);
+  todoItem.append(todoButtons);
+
+  return todoItem;
+}
+
+// addTodo
+todoFormElem.addEventListener('submit', e => {
+  e.preventDefault();
+  if (todoInputElem.value) {
+    const newItem = {
+      key: createDataKey(),
+      value: todoInputElem.value,
+      doned: false,
+    };
+    todos.items = [...todos.items, newItem];
+    todoInputElem.value = null;
+  }
+});
+
+// removeTodo
+const removeTodo = e => {
+  const target = e.target;
+  const todoItem = target.closest('li');
+  const key = Number(todoItem.dataset.key);
+
+  todoItem.hasAttribute('data-doned') && todos.count--;
+  todos.items = todos.items.filter(todo => key !== todo.key);
+};
+
+// doneTodo
+const doneTodo = e => {
+  const target = e.target;
+  const todo = target.closest('li');
+  const todoKey = Number(todo.dataset.key);
+
+  if (todo.hasAttribute('data-doned')) {
+    todo.removeAttribute('data-doned', '');
+    todos.count--;
+  } else {
+    todo.setAttribute('data-doned', '');
+    todos.count++;
+  }
+
+  target.classList.toggle('completed');
+
+  updatedTodoStatus(todoKey);
+};
+
+function updatedTodoValue(key, newText) {
+  todos.items = todos.items.map(todo => {
+    return todo.key === key ? { ...todo, value: newText } : todo;
+  });
+}
+
+function updatedTodoStatus(key) {
+  const todoUpdate = todos.items.map(todo => {
+    return todo.key === key ? { ...todo, doned: !todo.doned } : todo;
+  });
+
+  todos.items = todoUpdate.toSorted((a, b) => a.doned - b.doned);
+}
+
+// ==================================================== //
+// скрытие выполненных заданий
+// прикрутить к локальному хранилищу бы !
+
+const hiddenDoned = document.querySelector('.completedTasks');
+
+const hideСompletedTasks = () => {
+  const list = todoListElem.children;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].hasAttribute('data-doned')) {
+      list[i].classList.toggle('hidden');
+    }
+  }
+};
+
+hiddenDoned.addEventListener('change', e => {
+  hideСompletedTasks();
+});
+
+// ==================================================== //
+// редактирование уже созданных заданий
+
+const editTodo = e => {
+  const editBtn = e.target;
+  const todo = editBtn.closest('li');
+  const todoKey = Number(todo.dataset.key);
+  const todoText = todo.querySelector('p');
+
+  if (editBtn.hasAttribute('data-edit-active')) {
+    updatedTodoValue(todoKey, todoText.textContent);
+  } else {
+    editBtn.classList.add('btn_green');
+    editBtn.setAttribute('data-edit-active', '');
+
+    todoText.removeEventListener('click', doneTodo);
+    todoText.setAttribute('contentEditable', 'true');
+    todoText.focus();
+
+    document.addEventListener('keydown', e => {
+      if (e.key === 'Enter') {
+        updatedTodoValue(todoKey, todoText.textContent);
+      }
+    });
+  }
+};
